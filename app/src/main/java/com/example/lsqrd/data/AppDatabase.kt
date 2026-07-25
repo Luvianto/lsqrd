28de2879d9fc554dbf81a9c65cc54b8223457417
Luvianto <126235306+Luvianto@users.noreply.github.com>
2026-07-25 14:36:39 +0700

    Update AppDatabase.kt

--- app/src/main/java/com/example/lsqrd/data/AppDatabase.kt
+++ app/src/main/java/com/example/lsqrd/data/AppDatabase.kt
@@ -5,21 +5,31 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Entry::class], version = 1, exportSchema = false)
-abstract class AppDatabase: RoomDatabase() {
-    abstract fun entryDao(): EntryDao
+@Database(
+    entities = [
+        Vault::class,
+        Credential::class,
+        CredentialField::class
+    ],
+    version = 2,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun vaultDao(): VaultDao
+    abstract fun credentialDao(): CredentialDao
+    abstract fun credentialFieldDao(): CredentialFieldDao
 
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase{
-            return INSTANCE ?: synchronized(this){
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "lsqrd_db"
-                ).build()
+                ).fallbackToDestructiveMigration(false).build()
                 INSTANCE = instance
                 instance
             }
