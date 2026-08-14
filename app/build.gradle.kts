680ec8df4fad5dfc68f0c8f9e17a50fc67b39a39
Luvianto <126235306+Luvianto@users.noreply.github.com>
2026-08-14 15:06:05 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.biometric)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.sqlcipher)
 }
